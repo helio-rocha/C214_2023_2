755185e2e7c346e70d812cdd12eaad920d918a23
helio-rocha <66882296+helio-rocha@users.noreply.github.com>
2023-08-25 19:50:19 -0300

    Implementação das funcionalidades do desafio 2

--- Aula_1/Filme.js
+++ Aula_1/Filme.js
@@ -1,3 +1,86 @@
+const readline = require('readline');
+
+function askQuestion(query) {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+
+    var result = new Promise(resolve => rl.question( query, ans => {
+        rl.close();
+        resolve(ans);
+    }))
+
+    return result
+}
+
+var filmes = new Array();
+
+async function adicionarFilme()
+{
+    const nome = await askQuestion("Digite o nome do filme:");
+    const ano = await askQuestion("Digite o ano de lançamento do filme:");
+    const genero = await askQuestion("Digite o gênero do filme:");
+    const duracao = await askQuestion("Digite a duração do filme:");
+    const filme = new Filme(nome, ano, genero, duracao)
+    filmes.push(filme)
+    console.log('Filme criado\n')
+}
+
+async function marcarFilmeAssitido()
+{
+    var nome = await askQuestion("Digite o nome do filme que deseja marcar como assistido:");
+    var f;
+    await procurarFilme(nome).then(async filme => {
+        f = filme
+        f.marcarAssistido()
+        console.log('Filme assistido com sucesso\n')
+    }).catch(() => {
+        console.log('Filme não encontrado, tente novamente\n')
+    })
+}
+
+async function avaliarFilme()
+{
+    var nome = await askQuestion("Digite o nome do filme que deseja avaliar:");
+    var avaliacao;
+    var f
+    await procurarFilme(nome).then(async filme => {
+        f = filme
+        avaliacao = await askQuestion("Digite a nota do filme:");
+        f.avaliarFilme(avaliacao)
+        console.log('Filme avaliado com sucesso\n')
+    }).catch(() => {
+        console.log('Filme não encontrado, tente novamente\n')
+    })
+
+}
+
+async function exibirFilmes()
+{
+    filmes.forEach
+    (function (filme) 
+    {
+        filme.exibirDetalhes()
+    }
+    );
+}
+
+async function procurarFilme(nome)
+{
+    return new Promise((resolve, reject) => {
+        filmes.forEach
+        (function (filme) 
+        {
+            if (filme.titulo == nome)
+            {
+                resolve(filme)
+            }
+        }
+        );
+        reject()
+    })
+}
 class Filme
 {
     constructor(titulo, ano, genero, duracao, assistido, avaliacao)
@@ -6,8 +89,8 @@ class Filme
         this.ano = ano;
         this.genero = genero;
         this.duracao = duracao;
-        this.assistido = assistido;
-        this.avaliacao = avaliacao;
+        this.assistido = false;
+        this.avaliacao = null;
     }
 
     exibirDetalhes()
@@ -16,7 +99,7 @@ class Filme
         console.log(`Ano: ${this.ano}`);
         console.log(`Gênero: ${this.genero}`);
         console.log(`Duração: ${this.duracao}`);
-        if (this.avaliacao)
+        if (this.assistido == true)
         {
             console.log(`Assistido: SIM`);    
         }
@@ -24,7 +107,14 @@ class Filme
         {
             console.log(`Assistido: NÃO`);    
         }
-        console.log(`Avaliação: ${this.avaliacao}\n`);
+        if (this.avaliacao == null)
+        {
+            console.log(`Avaliação: Não avaliado\n`);
+        }
+        else
+        {
+            console.log(`Avaliação: ${this.avaliacao}\n`);
+        }
     }
 
     marcarAssistido()
@@ -38,19 +128,41 @@ class Filme
     }
 }
 
-const filme1 = new Filme('Morangos Silvestres', 1954, 'Drama', 91, false);
-const filme2 = new Filme('Taxi Driver', 1976, 'Drama', 114, false);
-const filme3 = new Filme('Solaris', 1972, 'Sci-Fi', 167, false);
+async function main()
+{
+    var flag = true
 
-filme1.marcarAssistido();
-filme1.avaliarFilme(97);
+    while (flag === true)
+    {
+        console.log("Selecione o que deseja fazer");
+        console.log("Digite 1 para adicionar um novo filme");
+        console.log("Digite 2 para marcar um filme como assistido");
+        console.log("Digite 3 para avaliar um filme");
+        console.log("Digite 4 para exibir a lista de filmes");
+        console.log("Digite 5 para sair do programa");
+        op = await askQuestion('')
+        switch (op)
+        {
+            case '1':
+                await adicionarFilme();
+                break
+            case '2':
+                await marcarFilmeAssitido();
+                break
+            case '3':
+                await avaliarFilme();
+                break
+            case '4':
+                await exibirFilmes();
+                break
+            case '5':
+                flag = false;
+                break
+            default:
+                console.log("Opção Inválida\n");
+                break;
+        }
+    }
+}
 
-filme2.marcarAssistido();
-filme2.avaliarFilme(98);
-
-filme3.marcarAssistido();
-filme3.avaliarFilme(99);
-
-filme1.exibirDetalhes();
-filme2.exibirDetalhes();
-filme3.exibirDetalhes();
+main()
